27490ad887edd624dcfdfcfd35dd4a5c17e3c4de
yu <yu@m.y-log.net>
2011-10-22 13:25:28 +0900

    add Device Elini (LGE L-04C)

---
+++ products/cyanogen_elini.mk
@@ -0,0 +1,44 @@
+# Inherit device configuration
+$(call inherit-product, device/lge/elini/elini.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+# Include GSM stuff
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+# Include FM-Radio stuff
+$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+
+PRODUCT_NAME := cyanogen_elini
+PRODUCT_BRAND := lge
+PRODUCT_DEVICE := elini
+PRODUCT_MODEL := L-04C
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_elini BUILD_ID=FRG83G BUILD_DISPLAY_ID=FRG83G BUILD_FINGERPRINT=LGE/elini/elini/elini:2.2.2/FRG83G/eng.user.20110215.121815:user/release-keys PRIVATE_BUILD_DESC="elini-user 2.2.2 FRG83G eng.user.20110215.121815 release-keys"
+
+# Extra Star overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/elini
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-elini
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-elini
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.1.0-elini-KANG
+    endif
+endif
+
+#
+# Copy prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
+
